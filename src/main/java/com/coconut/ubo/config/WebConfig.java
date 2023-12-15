946bb14abe2d2b47ec6e4a3c7f5a0159f371ef92
sheepjieun <sheepjieun@gmail.com>
2023-12-15 10:33:53 +0900

    fix:

--- src/main/java/com/coconut/ubo/config/WebConfig.java
+++ src/main/java/com/coconut/ubo/config/WebConfig.java
@@ -1,8 +1,14 @@
 package com.coconut.ubo.config;
 
+import com.coconut.ubo.web.argumentresolver.LoginUserArgumentResolver;
+import com.coconut.ubo.web.interceptor.LogInterceptor;
+import com.coconut.ubo.web.interceptor.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 
 /**
@@ -24,26 +30,27 @@ public class WebConfig implements WebMvcConfigurer {
                 .maxAge(3600); // 1 hour
     }
 
-//    // Argument Resolver 추가
-//    @Override
-//    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-//        resolvers.add(new LoginUserArgumentResolver());
-//    }
-//
-//    // Interceptor 추가
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(new LogInterceptor()) // 인터셉터 등록
-//                .order(1) // 인터셉터 호출 순서 지정. 낮을 수록 먼저 호출
-//                .addPathPatterns("/**") // 인터셉터를 적용할 URL 패턴 지정
-//                .excludePathPatterns("/css/**", "/*.ico", "/error"); // 인터셉터제어 제외할 패턴 지정
-//
-//        registry.addInterceptor(new LoginCheckInterceptor())
-//                .order(2)
-//                .addPathPatterns("/**")
-//                .excludePathPatterns("/signup", "/login", "/logout",
-//                        "/css/**", "/*.ico", "/error" );
-//        WebMvcConfigurer.super.addInterceptors(registry);
-//    }
+    // Argument Resolver 추가
+    @Override
+    public void addArgumentResolvers(List<org.springframework.web.method.support.HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginUserArgumentResolver());
+    }
+
+    // Interceptor 추가
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor()) // 인터셉터 등록
+                .order(1) // 인터셉터 호출 순서 지정. 낮을 수록 먼저 호출
+                .addPathPatterns("/**") // 인터셉터를 적용할 URL 패턴 지정
+                .excludePathPatterns("/css/**", "/*.ico", "/error"); // 인터셉터제어 제외할 패턴 지정
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/signup", "/login", "/logout",
+                        "/css/**", "/*.ico", "/error" );
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
+
 
 }
